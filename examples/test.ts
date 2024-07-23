4c317f41d66b01b4da11b2d36a1785ad68d6c04f
Jonas Sobotta <jonas.sobotta01@gmail.com>
2024-07-23 17:39:24 +0200

    Add uuid to identifier transformation

--- examples/test.ts
+++ examples/test.ts
@@ -1,5 +1,14 @@
 const myVariable = true;
+const anotherVariable = 5;
+const andAnotherVariable = "Hello";
+const someNumberVariable = 5.5;
 function greet() {
   console.log("Greetings!");
 }
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
 greet();
+add(anotherVariable, someNumberVariable);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as ts from 'typescript';
+import { v4 as uuidv4 } from 'uuid';
 
 const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
     function visitor(node: ts.Node): ts.Node {
       // Rename all identifiers to "foo"
       if (ts.isIdentifier(node)) {
-        return ts.factory.createIdentifier('foo');
+        return ts.factory.createIdentifier(`foo${uuidv4().replace(/-/g, '')}`);
       }
 
       return ts.visitEachChild(node, visitor, context);
