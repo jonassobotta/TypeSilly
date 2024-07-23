3fa327d8f946d8f84af6d25f6cf48068cab9fd62
Jonas Sobotta <76175870+jonassobotta@users.noreply.github.com>
2024-07-23 21:38:48 +0200

    Test boolean switcher (#2)
    * Add BooleanSwitcher
    
    * Add unit test for booleanSwitcher and update package.json

--- compile.ts
+++ compile.ts
@@ -1,5 +1,5 @@
 import * as ts from 'typescript';
-import transformer from './src/index';
+import combinedTransformer from './src/index';
 
 const fileNames = process.argv.slice(2);
 
@@ -9,6 +9,24 @@ const program = ts.createProgram(fileNames, {
   target: ts.ScriptTarget.ES5,
 });
 
-program.emit(undefined, undefined, undefined, undefined, {
-  before: [transformer],
+const transformers: ts.CustomTransformers = {
+  before: [combinedTransformer as ts.TransformerFactory<ts.SourceFile>],
+  after: []
+};
+
+const emitResult = program.emit(undefined, undefined, undefined, undefined, transformers);
+
+const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+allDiagnostics.forEach(diagnostic => {
+  if (diagnostic.file) {
+    const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+    const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+    console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+  } else {
+    console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+  }
 });
+
+const exitCode = emitResult.emitSkipped ? 1 : 0;
+process.exit(exitCode);
--- src/idnetifierRenamer.ts
+++ src/idnetifierRenamer.ts
@@ -2,17 +2,15 @@ import * as ts from 'typescript';
 import { v4 as uuidv4 } from 'uuid';
 
 const identifierRenamer: ts.TransformerFactory<ts.SourceFile> = context => {
-    return sourceFile => {
-        function visitor(node: ts.Node): ts.Node {
-            if (ts.isIdentifier(node)) {
-            const uniqueId = `foo_${uuidv4().replace(/-/g, '')}`;
-            return ts.factory.createIdentifier(uniqueId);
-            }
-            return ts.visitEachChild(node, visitor, context);
-        }
-  
-        return ts.visitNode(sourceFile, visitor) as ts.SourceFile;
-    };
+  const visit: ts.Visitor = node => {
+    if (ts.isIdentifier(node)) {
+      const uniqueId = `foo_${uuidv4().replace(/-/g, '')}`;
+      return ts.factory.createIdentifier(uniqueId);
+    }
+    return ts.visitEachChild(node, visit, context);
+  };
+
+  return sourceFile => ts.visitNode(sourceFile, visit) as ts.SourceFile;
 };
 
 export default identifierRenamer;
